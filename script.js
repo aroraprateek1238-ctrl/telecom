1023e6820e4912723c9627958553ae4a61ec16ff
aroraprateek1238-ctrl <arora.prateek1238@gmail.com>
2025-09-04 03:57:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ if (document.getElementById('cart-items')) {
     // Delay navigation just a bit
   setTimeout(() => {
     window.location.href = "thankyou.html";
-  }, 2000); // 300ms = invisible to user, enough for GA
+  }, 10000); // 300ms = invisible to user, enough for GA
     // window.location.href = "thankyou.html";
   });
 }
